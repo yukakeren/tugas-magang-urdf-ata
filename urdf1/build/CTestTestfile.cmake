0d7c31f16272dc18b6dd38ae12ced2d5126fd4dd
yukakeren <ataaqiela.kamil@gmail.com>
2024-11-05 19:41:04 +0700

    tugas 1

---
+++ urdf1/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukakeren/Documents/urdf1/src
+# Build directory: /home/yukakeren/Documents/urdf1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arm_controller")
+subdirs("robot_description")
