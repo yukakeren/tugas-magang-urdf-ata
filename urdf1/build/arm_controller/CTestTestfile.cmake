8f585e14240a28ca4664082024ff9cd5a47dd132
yukakeren <ataaqiela.kamil@gmail.com>
2024-11-05 19:57:22 +0700

    tugas 1

---
+++ urdf1/build/arm_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukakeren/Documents/urdf1/src/arm_controller
+# Build directory: /home/yukakeren/Documents/urdf1/build/arm_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
